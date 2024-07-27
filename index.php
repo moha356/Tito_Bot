81787d7e08dc01b46ef5c7b7913ef7365284aa2d
moha356 <76640937+moha356@users.noreply.github.com>
2024-07-27 13:01:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 #BOT CREADO POR  Andresitoww
-$botToken = '5359484704:AAH-jUHJcjoCp05qrV5RuwEL1Is35V3KU9g'; #AGREGAMOS EL TOKEN DEL BOT Aqui Ponemos El 
+$botToken = '7053591666:AAEGdnSi_2NrhjOecCsxeFofUMzG_glX9r4'; #AGREGAMOS EL TOKEN DEL BOT Aqui Ponemos El 
 $website  = "https://api.telegram.org/bot".$botToken;
 $update   = file_get_contents('php://input');
 $update   = json_decode($update, true);
